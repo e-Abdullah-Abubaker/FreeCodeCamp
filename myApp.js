a00f61347ddc23282d4e8722b3fc5f7b4e70cf0a
e-Abdullah-Abubaker <abdullah.abubaker@emumba.com>
2024-07-29 06:13:50 +0000

    task 4

--- myApp.js
+++ myApp.js
@@ -1,4 +1,33 @@
 let express = require("express");
 let app = express();
 
+absolutePath = __dirname + "/views/index.html";
+
+app.get("/json", (req, res) => {
+  res.json({
+    message:
+      process.env.MESSAGE_STYLE == "uppercase" ? "HELLO JSON" : "Hello Json",
+  });
+});
+
+// Task 4
+// app.use("/public", express.static(__dirname + "/public"));
+
+// app.get("/", (req, res) => {
+//   res.sendFile(absolutePath);
+// });
+
+// Task 3
+// app.get("/", (req, res) => {
+//   res.sendFile(absolutePath);
+// });
+
+// Task 2
+// app.Get("/", (req, res) => {
+//     res.send("Hello Express");
+// }
+
+//Task 1
+// console.log("Hello World")
+
 module.exports = app;
